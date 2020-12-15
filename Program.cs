7bcea058efafe2960c42dcb2a5e61869dbb1d08f
Speaqer <speaqerpr0@gmail.com>
2020-12-16 01:39:32 +0200

    Use .NET 5

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+namespace HabboGallery.SchemeHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) return;
+            byte[] data = Encoding.ASCII.GetBytes(args[0]);
+
+            using var client = new UdpClient("127.0.0.1", 9119);
+            client.Send(data, data.Length);
+        }
+    }
+}
