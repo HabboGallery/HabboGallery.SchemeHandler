0317518c54ef0310a82f1db40db69c8742f3a898
Speaqer <speaqerpr0@gmail.com>
2021-10-27 20:09:21 +0300

    Use .NET 6

--- Program.cs
+++ Program.cs
@@ -1,17 +1,7 @@
 ﻿using System.Text;
 using System.Net.Sockets;
 
-namespace HabboGallery.SchemeHandler
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length < 1) return;
-            byte[] data = Encoding.ASCII.GetBytes(args[0]);
+if (args.Length < 1) return;
 
-            using var client = new UdpClient("127.0.0.1", 9119);
-            client.Send(data, data.Length);
-        }
-    }
-}
+using var client = new UdpClient("127.0.0.1", 9119);
+client.Send(Encoding.ASCII.GetBytes(args[0]));
