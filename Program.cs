cb3fda9b5a9dd5b9691fd95b56d03db89694403b
PaulusParssinen <paulus.parssinen@gmail.com>
2022-06-27 23:27:23 +0300

    Use .NET 7, NativeAOT and trimming!

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@ using System.Net.Sockets;
 
 if (args.Length < 1) return;
 
-using var client = new UdpClient("127.0.0.1", 9119);
-client.Send(Encoding.ASCII.GetBytes(args[0]));
+using var socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+socket.Connect("127.0.0.1", 9119);
+socket.Send(Encoding.ASCII.GetBytes(args[0]));
